1f9d490691d94b9b673594651d4dee89475e999a
Andrea Ochoa <andreavivianaochoa@gmail.comexit>
2026-05-22 12:23:57 -0300

    Texto ver carrito agregado

--- src/componentes/CartWidget/CartWidget.jsx
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
 
 function CartWidget() {
-  const { cantidadTotal } = useContext(CartContext);
+    const { cantidadTotal } = useContext(CartContext);
 
-  return (
-    <Link
-      to="/carrito"
-      style={{
-        textDecoration: "none",
-        color: "#7c4dff",
-        fontWeight: "bold"
-      }}
-    >
-      🛒 Ver carrito ({cantidadTotal})
-    </Link>
-  );
+    return (
+        <Link
+            to="/carrito"
+            style={{
+                textDecoration: "none",
+                color: "#7c4dff",
+                fontWeight: "bold"
+            }}
+        >
+            🛒 Ver carrito ({cantidadTotal})
+        </Link>
+    );
 }
 
 export default CartWidget;
