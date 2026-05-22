61854e3e888d634004e8679092a16920e4259d20
Andrea Ochoa <andreavivianaochoa@gmail.comexit>
2026-05-22 16:12:49 -0300

    Nuevo header con logo y responsive corregido

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -7,13 +7,19 @@ function Header() {
   return (
     <header className={styles.header}>
 
-      <h1 className={styles.titulo}>
-        Material didáctico 🛍️
-      </h1>
+      <div className={styles.superior}>
 
-      <p className={styles.subtitulo}>
-        Lecturas, materiales y secuencias
-      </p>
+        <img
+          src="/images/logo.png"
+          alt="Caja Didáctica"
+          className={styles.logo}
+        />
+
+        <p className={styles.subtitulo}>
+          Lecturas, materiales y secuencias
+        </p>
+
+      </div>
 
       <nav className={styles.nav}>
 
@@ -23,14 +29,14 @@ function Header() {
           <Link to="/cuadernillos">Cuadernillos</Link>
           <Link to="/fechas">Fechas Especiales</Link>
           <Link to="/juegos">Juegos</Link>
-           <Link to="/alta-producto">Alta Producto</Link>
+          <Link to="/alta-producto">Alta Producto</Link>
           <Link to="/contacto">Contacto</Link>
-         
+
         </div>
 
-       <div className={styles.carrito}>
-  <CartWidget />
-</div>
+        <div className={styles.carrito}>
+          <CartWidget />
+        </div>
 
       </nav>
 
